82246b729bea357dc4770a830542998070e9cf09
Keith Hughitt <keith.hughitt@gmail.com>
2022-05-30 09:03:44 -0700

    speed up chemical/pmid mapping step

--- scripts/create_chemical_pmid_mapping.py
+++ scripts/create_chemical_pmid_mapping.py
@@ -8,6 +8,21 @@ import pandas as pd
 # load chemical data
 chemical_dat = pd.read_feather(snakemake.input[0])
 
+# convert concept id to pyarrow/string type (~7x faster in testing..)
+chemical_dat.concept_id = chemical_dat.concept_id.astype('string[pyarrow]')
+
+# baseline
+# %timeit chemical_dat.concept_id == mesh_id
+# 3.13 s ± 49.2 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
+
+# string
+# %timeit chemical_dat.concept_id == mesh_id
+# 4.25 s ± 33.5 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
+
+# string[pyarrow]
+# %timeit  chemical_dat.concept_id == mesh_id
+# 428 ms ± 48.3 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
+
 # iterate over chemicals
 mesh_ids = list(chemical_dat.concept_id.unique())
 num_chemicals = len(mesh_ids)
@@ -15,8 +30,12 @@ num_chemicals = len(mesh_ids)
 # iterate over chemicals and retrieve associated pubmed ids for each
 chemical_pmids = {}
 
-for mesh_id in mesh_ids:
+for i, mesh_id in enumerate(mesh_ids):
+    print(f"Processing MeSH ID {i + 1}/{len(mesh_ids)}")
+
+    # slow step..
     mask = chemical_dat.concept_id == mesh_id
+
     chemical_pmids[mesh_id] = set(chemical_dat[mask].pmid.values)
 
 # encoder to convert int64 elements to generic ints and sets to lists during
